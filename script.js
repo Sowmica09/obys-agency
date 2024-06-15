f6af2bd702857147e9a4021aef63cc9441e40cea
Sowmica09 <sowmica99@gmail.com>
2024-06-15 11:44:54 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+function loadingAnimation(){  
 var tl = gsap.timeline()
 tl.from(".line h1",{
     y:150,
@@ -28,14 +28,35 @@ tl.to(".line h2",{
 tl.to("#loader",{
     opacity:0,
     duration:0.2,
-    delay:4
+    delay:2
 })
 tl.from("#page1",{
-    y:1200,
-    opacity:0,
+    y:1600,
+    duration:0.6,
     delay:0.2,
     ease:Power4
 })
 tl.to("#loader",{
     display:"none"
 })
+tl.from("#nav",{
+    opacity:0
+})
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+    y:150,
+    stagger:0.2
+})
+}
+function cursorAnimation(){
+    document.addEventListener("mousemove",function (dets) {
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y
+        })
+    })
+    
+    Shery.makeMagnet("#nav-part2 h4", {
+           });
+}
+loadingAnimation();
+cursorAnimation()
